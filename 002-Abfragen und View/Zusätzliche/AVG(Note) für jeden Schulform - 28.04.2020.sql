9a447acbfa659c12a8bf2c07eb49ddcd24e1313e
Adeline Makoudjou <makoudjou@gmail.com>
2020-05-26 07:36:37 +0200

    Delete AVG(Note) für jeden Schulform - 28.04.2020.sql

--- 002-Abfragen und View/Zusätzliche/AVG(Note) für jeden Schulform - 28.04.2020.sql
+++
@@ -1,5 +0,0 @@
-SELECT Schulform_Qualifikation.Schulform_Bezeichnung, AVG(tb_Ergebnisse.Note)
-FROM tb_Ergebnisse
-RIGHT JOIN tb_Klassen ON tb_Ergebnisse.Klassen_ID = tb_Klassen.Klassen_ID
-RIGHT JOIN Schulform_Qualifikation ON Schulform_Qualifikation.Schulform_ID = tb_Klassen.Schulform_ID
-GROUP BY Schulform_Qualifikation.Schulform_Bezeichnung
